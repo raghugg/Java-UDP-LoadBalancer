34a9041fbacc33752de9145e398480e3156a9708
raghugg <guggilam.raghu@gmail.com>
2026-04-06 21:12:31 -0400

    add test client

---
+++ src/main/java/com/loadbalancer/client/Client.java
@@ -0,0 +1,30 @@
+package com.loadbalancer.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        String host = args.length > 0 ? args[0] : "localhost";
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 8080;
+        String message = args.length > 2 ? args[2] : "Hello from client";
+
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Connected to " + host + ":" + port);
+            out.println(message);
+            out.println(); // blank line signals end of request
+
+            String response;
+            while ((response = in.readLine()) != null) {
+                System.out.println("Response: " + response);
+            }
+        }
+    }
+}
